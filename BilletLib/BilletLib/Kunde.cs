f18c368c643fdadd4f8475d5e903179609830e58
Fridai <Nikolaiviby3@gmail.com>
2018-02-01 16:07:54 +0100

    Bonus opgave.

---
+++ BilletLib/BilletLib/Kunde.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace BilletLib
+{
+    
+    public class Kunde
+    {
+        private List<Vehicle> _ture;
+
+        public Kunde()
+        {
+            _ture = new List<Vehicle>();
+
+            AddNormalCarTrip();
+            AddNormalCarTripWithBroBizz();
+            AddNormalMCTrip();
+            AddNormalMCTripWithBroBizz();
+            AddØresundCarTrip();
+            AddØresundCarTripWithBroBizz();
+            AddØresundMCTrip();
+            AddØresundMCTripWithBroBizz();
+        }
+
+        public void AddNormalCarTrip()
+        {
+            _ture.Add(new Bil("asdh", false));
+        }
+
+        public void AddNormalCarTripWithBroBizz()
+        {
+            _ture.Add(new Bil("asdh", true));
+        }
+
+        public void AddØresundCarTrip()
+        {
+            _ture.Add(new Bil("asdh", false, true));
+        }
+
+        public void AddØresundCarTripWithBroBizz()
+        {
+            _ture.Add(new Bil("asdh", true, true));
+        }
+
+        public void AddNormalMCTrip()
+        {
+            _ture.Add(new MC("asdh", false));
+        }
+
+        public void AddNormalMCTripWithBroBizz()
+        {
+            _ture.Add(new MC("asdh", true));
+        }
+
+        public void AddØresundMCTrip()
+        {
+            _ture.Add(new MC("asdh", false, true));
+        }
+
+        public void AddØresundMCTripWithBroBizz()
+        {
+            _ture.Add(new MC("asdh", true, true));
+        }
+
+        public double SumOfTrips()
+        {
+            double sum = 0;
+            foreach (Vehicle v in _ture)
+            {
+                sum = sum + v.Pris();
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ BilletLib/BilletLibTests/KundeTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+    [TestClass()]
+    public class KundeTests
+    {
+        [TestMethod()]
+        public void KundeTest()
+        {
+            Kunde k = new Kunde();
+
+            Assert.AreEqual(1565.75, k.SumOfTrips());
+        }
+    }
+}
